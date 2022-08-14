f953d9e9635786600eda202624e0dbfd4aebb2a9
ayaka24 <ayaka.sunakawa24@gmail.com>
2022-08-14 17:32:17 +0900

    secont commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <p className="text-4xl">Hello Next.js</p>
+    </Layout>
+  );
+}
